104ca703bb3c0e2522c7b34d5c57f80452165a10
Riccardo Rasera <riccardo.rasera.extern@adesso.it>
2026-05-10 17:35:10 +0200

    Added tags to Grafana tests and updated pipeline's action versions

--- grafana/requests/accounts.js
+++ grafana/requests/accounts.js
@@ -7,50 +7,64 @@ const ACCOUNTS_PATH = "/accounts";
 
 export function searchAccounts() {
     const page = Math.floor(Math.random() * 1001)
-    const res = http.get(`${BASE_URL}${ACCOUNTS_PATH}/search?page=${page}&size=20`, {headers: HEADERS});
+
+    const res = http.get(`${BASE_URL}${ACCOUNTS_PATH}/search?page=${page}&size=20`, {
+        headers: HEADERS,
+        tags: {name: "SearchAccounts"}
+    });
 
     check(res, {
-        [`GET ${ACCOUNTS_PATH} status 200`]: successHandler
+        "GET /accounts/search status 200": successHandler
     });
 
     return res;
 }
 
 export function getAccountById(id) {
-    const res = http.get(`${BASE_URL}${ACCOUNTS_PATH}/${id}`, {headers: HEADERS});
-
-    check(res, {
-        [`GET ${ACCOUNTS_PATH}/${id} status 200`]: successHandler
+    const res = http.get(`${BASE_URL}${ACCOUNTS_PATH}/${id}`, {
+        headers: HEADERS,
+        tags: {name: "GetAccountById"}
     });
+
+    check(res, {"GET /accounts/{id} status 200": successHandler});
 
     return res;
 }
 
 export function createAccount(payload) {
-    const res = http.post(`${BASE_URL}${ACCOUNTS_PATH}`, JSON.stringify(payload), {headers: HEADERS});
+    const res = http.post(`${BASE_URL}${ACCOUNTS_PATH}`, JSON.stringify(payload), {
+        headers: HEADERS,
+        tags: {name: "CreateAccount"}
+    });
 
     check(res, {
-        [`POST ${ACCOUNTS_PATH} status 201`]: createdHandler
+        "POST /accounts status 201": createdHandler
     });
 
     return res;
 }
 
 export function patchAccount(id, payload) {
-    const res = http.patch(`${BASE_URL}${ACCOUNTS_PATH}/${id}`, JSON.stringify(payload), {headers: HEADERS});
+    const res = http.patch(`${BASE_URL}${ACCOUNTS_PATH}/${id}`, JSON.stringify(payload), {
+        headers: HEADERS,
+        tags: {name: "PatchAccount"}
+    });
 
     check(res, {
-        [`PATCH ${ACCOUNTS_PATH} status 200`]: successHandler
+        "PATCH /accounts/{id} status 200": successHandler
     });
 
     return res;
 }
 
 export function deleteAccount(id) {
-    const res = http.del(`${BASE_URL}${ACCOUNTS_PATH}/${id}`, null, {headers: HEADERS});
+    const res = http.del(`${BASE_URL}${ACCOUNTS_PATH}/${id}`, null, {
+        headers: HEADERS,
+        tags: {name: "DeleteAccount"}
+    });
 
     check(res, {
-        [`DELETE ${ACCOUNTS_PATH}/${id} status 204`]: noContentHandler
+        "DELETE /accounts/{id} status 204": noContentHandler
     });
 
     return res;
